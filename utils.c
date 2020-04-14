3616f07810929148ff9c31273a410f6c8ef7eeaa
mpulido19 <60494640+mpulido19@users.noreply.github.com>
2020-04-14 16:47:32 -0700

    Add files via upload

---
+++ utils.c
@@ -0,0 +1,11 @@
+#include "utils.h"
+
+
+//-------------------------------------------------
+const char* yesorno(bool condition) {
+  static char yes[] = "YES";
+  static char no[]  = "NO";
+
+  return condition ? yes : no;
+}
+
---
+++ utils.h
@@ -0,0 +1,9 @@
+#ifndef utils_h
+#define utils_h
+
+#include <stdbool.h>
+
+const char* yesorno(bool condition);
+
+
+#endif /* utila_h */
